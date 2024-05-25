9f9e65d43ccdbb941793253643328775d9cd63da
zxcursed2008 <158029793+zxcursed2008@users.noreply.github.com>
2024-05-25 10:46:26 +0300

    Update and rename market.js to market_final.js

---
+++ Final_GameMarket/market_final.js
@@ -0,0 +1,317 @@
+
+let ItemsArray = [
+    {
+        title: "Дід проти москалів",
+        author: "Боровий Артур",
+        author_photo: "Боровий.jpg",
+        email: "23a_bam@liceum.ztu.edu.ua",
+        group: "10-А-1",
+        image: "Боровий_гра.png",
+        game_github: "https://github.com/23a-bam/Phaser2ndGame",
+        game_pages: "https://23a-bam.github.io/Phaser2ndGame/",
+        docs: "https://drive.google.com/drive/folders/1A12wx0lfNj7Aef9KAw2CFMaDwhWM8V4Q?usp=sharing",
+        forms: "https://forms.gle/sS7tuJWJuGDjmHB98",
+        gamemarket_github: "https://github.com/23a-bam/GameMarket_Borovyi",
+        gamemarket_pages: "https://23a-bam.github.io/GameMarket_Borovyi/",
+        market_pages: "https://23a-bam.github.io/GameMarket_Borovyi/market.html",
+        stars: 4
+    },
+    {
+        title: "Магазин UkraineRog",
+        author: "Геча Кирило",
+        author_photo: "Геча.jpg",
+        email: "23a_gki@liceum.ztu.edu.ua",
+        group: "10-А-1",
+        image: "Геча_гра.png", 
+        game_github: "https://github.com/Kyrylooo/GameMarket_Surname",
+        game_pages: "https://kyrylooo.github.io/Test1/",
+        docs: "",
+        forms: "",
+        gamemarket_github: "https://github.com/Kyrylooo/GameMarket_Surname",
+        gamemarket_pages: "https://kyrylooo.github.io/Test1/",
+        market_pages: "https://kyrylooo.github.io/Test1/market.html",
+        stars:3
+    },
+    {
+        title: "",
+        author: "Громова Марина",
+        author_photo: "Громова.jpg",
+        email: "23a_gmo@liceum.ztu.edu.ua",
+        group: "10-А-1",
+        image: "Громова_гра.png",
+        game_github: "https://github.com/Marina745/Phaser2ndGame",
+        game_pages: "https://marina745.github.io/Phaser2ndGame/",
+        docs: "https://drive.google.com/drive/folders/1h8T6v1Yt0wkzkiI72lSMMZndaidptT6o",
+        forms: "https://forms.gle/4tsVxrHtBpkLLmdc6",
+        gamemarket_github: "https://github.com/Marina745/GameMarket_Hromova",
+        gamemarket_pages: "https://marina745.github.io/GameMarket_Hromova/market.html",
+        market_pages: ""
+    },
+    {
+        title: "",
+        author: "Гуменюк Аріна",
+        author_photo: "Гуменюк.jpg",
+        email: "23a_gayu@liceum.ztu.edu.ua",
+        group: "10-А-1",
+        image: "Гуменюк_гра.png",
+        game_github: "https://github.com/NoraBlank/-Phaser2ndGame?authuser=0",
+        game_pages: "https://norablank.github.io/-Phaser2ndGame/?authuser=0",
+        docs: "https://docs.google.com/document/d/1ESxlcNrGJPWx--NdlFbMdZJNQ3mHWVKQ-HktIVOJBVY/edit?usp=sharing",
+        forms: "https://forms.gle/rNroPXhu2mFSxcHx8",
+        gamemarket_github: "",
+        gamemarket_pages: "",
+        market_pages: ""
+    },
+    {
+        title: "Дід Мороз Альбатрос",
+        author: "Демидчук Ілля",
+        author_photo: "Демидчук.jpg", 
+        email: "23a_dii@liceum.ztu.edu.ua",
+        group: "10-А-1",
+        image: "Демидчук_гра.png", 
+        game_github: "https://github.com/zxcursed2008/Phaser2ndGame",
+        game_pages: "https://zxcursed2008.github.io/Phaser2ndGame/",
+        docs: "https://drive.google.com/drive/folders/1BfaZ86pNTA3FO_rOn196XXKOLYNQQ81E",
+        forms: "https://forms.gle/xs7rgVauTR9ryuNc7",
+        gamemarket_github: "https://github.com/zxcursed2008/GameMarket_Demydchuk/",
+        gamemarket_pages: "https://zxcursed2008.github.io/GameMarket_Demydchuk/",
+        market_pages: "https://zxcursed2008.github.io/GameMarket_Demydchuk/market.html"
+    },
+    {
+        title: "Царівна жаба",
+        author: "Деркач Єлизавета",
+        author_photo: "Деркач.jpg",
+        email: "23a_dyem@liceum.ztu.edu.ua",
+        group: "10-А-1",
+        image: "Деркач_гра.png",
+        game_github: "https://github.com/liska228/Phaser2ndGame",
+        game_pages: "https://liska228.github.io/Phaser2ndGame/",
+        docs: "https://drive.google.com/drive/folders/1uFrc-z8MnRAFvR7sPNM1NnN800TJOUfr",
+        forms: "https://forms.gle/XDzvrjzJhjq758XFA",
+        gamemarket_github: "https://github.com/liska228/GameMarket_Derkach-",
+        gamemarket_pages: "https://liska228.github.io/GameMarket_Derkach-/",
+        market_pages: "https://liska228.github.io/GameMarket_Derkach-/market.html"
+    },
+    {
+        title: "Пригоди Свинки Шмеппи 2",
+        author: "Євланов Данііл",
+        author_photo: "Євланов.jpg",
+        email: "23a_yedv@liceum.ztu.edu.ua",
+        group: "10-А-1",
+        image: "Євланов_гра.png",
+        game_github: "https://github.com/Scoxxy/Phaser2ndGame",
+        game_pages: "https://scoxxy.github.io/Phaser2ndGame/",
+        docs: "https://drive.google.com/drive/folders/1IzTICIYe3e_rj81-ihjd3oDOz6xfSYQR?usp=drive_link",
+        forms: "https://forms.gle/vWZ2hn9MopmMy5fc7",
+        gamemarket_github: "https://github.com/Scoxxy/GameMarket_YevlanovDaniil",
+        gamemarket_pages: "https://scoxxy.github.io/GameMarket_YevlanovDaniil/",
+        market_pages: "https://scoxxy.github.io/GameMarket_YevlanovDaniil/market.html"
+    },
+    {
+        title: "Червона відьма",
+        author: "Катя Булич",
+        author_photo: "Булич.jpg",
+        email: "23a_bks@liceum.ztu.edu.ua",
+        group: "10-А-1",
+        image: "Булич_гра.png",
+        game_github: "https://github.com/katyablchh/Phaser2ndGame",
+        game_pages: "https://katyablchh.github.io/Phaser2ndGame/",
+        docs: "https://drive.google.com/drive/folders/1bNoYv5ChnzJTrIq0fjekfbqMNjz4SZ40?usp=sharing",
+        forms: "https://forms.gle/dxTCp9vfCCgZwyy97",
+        gamemarket_github: "https://github.com/katyablchh/GameMarket_Bulych",
+        gamemarket_pages: "https://katyablchh.github.io/GameMarket_Bulych/",
+        market_pages: "https://katyablchh.github.io/GameMarket_Bulych/finalgamemarket/market.html"
+    },
+    {
+        title: "They alive",
+        author: "Ковальов Андрій",
+        author_photo: "Ковальов.jpg",
+        email: "23a_kai@liceum.ztu.edu.ua",
+        group: "10-А-1",
+        image: "Ковальов_гра.png",
+        game_github: "https://github.com/Andre00785/Phaser1stGame",
+        game_pages: "https://github.com/Andre00785/Phaser1stGame/settings/pages",
+        docs: "https://docs.google.com/document/d/1D1bRBFti1aHsr-ZAqkfDkK9tdEeOpkM26E9qyWCE8O0/edit?usp=sharing",
+        forms: "https://forms.gle/i7YyjwzaHp7QvXei8",
+        gamemarket_github: "https://github.com/Andre00785/Game_Market_Kovalev",
+        gamemarket_pages: "https://andre00785.github.io/Game_Market_Kovalev/",
+        market_pages: "https://andre00785.github.io/Game_Market_Kovalev/market.html"
+    },
+    {
+        title: "Catventure",
+        author: "Кожанов Юрій",
+        author_photo: "Кожанов.jpg",
+        email: "23a_kyuk@liceum.ztu.edu.ua",
+        group: "10-А-1",
+        image: "Кожанов_гра.png",
+        game_github: "https://github.com/kyuk278937/Catventure",
+        game_pages: "",
+        docs: "https://drive.google.com/drive/folders/1GM-292zUzJSJlIB4-4_CIoOsHalliizh?usp=sharing",
+        forms: "https://forms.gle/NN9okEBmpsLw3AmX8",
+        gamemarket_github: "https://github.com/kyuk278937/GameMarket",
+        gamemarket_pages: "https://kyuk278937.github.io/GameMarket/",
+        market_pages: "https://kyuk278937.github.io/-HomeAppliancesStore/"
+    },
+    {
+        title: "Пригоди червоної шапочки",
+        author: "Козлюк Марина",
+        author_photo: "Козлюк.jpg",
+        email: "23a_kmo@liceum.ztu.edu.ua",
+        group: "10-А-1",
+        image: "Козлюк_гра.png",
+        game_github: "https://github.com/Marykozl/Phaser2ndGame",
+        game_pages: "https://marykozl.github.io/Phaser2ndGame/",
+        docs: "https://drive.google.com/drive/folders/1AZzV2iit4QmQfotZM68HmWo4OjG-5ikO",
+        forms: "https://forms.gle/PdXpRXZrbn9ujeMH7",
+        gamemarket_github: "https://github.com/Marykozl/GameMarket_Kozlyuk",
+        gamemarket_pages: "https://marykozl.github.io/GameMarket_Kozlyuk/",
+        market_pages: "https://marykozl.github.io/GameMarket_Kozlyuk/market.html"
+    },
+    {
+        title: "Пригоди Свиноти",
+        author: "Кравчук Іван",
+        author_photo: "Кравчук.jpg",
+        email: "23a_kia@liceum.ztu.edu.ua",
+        group: "10-А-1",
+        image: "Кравчук_гра.png",
+        game_github: "https://github.com/maskenissen1/Phaser2ndGame-1",
+        game_pages: "https://maskenissen1.github.io/Phaser2ndGame-1/",
+        docs: "https://drive.google.com/drive/folders/1_qC1jSGlqtfs16m1_nriWvomLg51Ah25",
+        forms: "https://forms.gle/S2YEZ1dn7tYTkrbF6",
+        gamemarket_github: "https://github.com/maskenissen1/GameMarket_Kravchuk",
+        gamemarket_pages: "https://maskenissen1.github.io/GameMarket_Kravchuk/",
+        market_pages: "https://maskenissen1.github.io/GameMarket_Kravchuk/market.html"
+    },
+    {
+        title: "Mavka",
+        author: "Лазаренко Софія",
+        author_photo: "Лазаренко.jpg",
+        email: "23a_lsa@liceum.ztu.edu.ua",
+        group: "10-А-1",
+        image: "Лазаренко_гра.png",
+        game_github: "https://github.com/SofiaLazarenko/Phaser2ndGame?authuser=0",
+        game_pages: "https://sofialazarenko.github.io/Phaser2ndGame/?authuser=0",
+        docs: "https://docs.google.com/document/d/1Dqe-8TcUA1YLj7mG-2IMBxHsPmlhkwfkEfrOTAb9Nvs/edit?usp=classroom_web&authuser=0",
+        forms: "https://forms.gle/PgJv2LH1ZJjjS29V8",
+        gamemarket_github: "https://github.com/SofiaLazarenko/GameMarket_Lazarenko?authuser=0",
+        gamemarket_pages: "https://sofialazarenko.github.io/GameMarket_Lazarenko/index.html",
+        market_pages: "https://sofialazarenko.github.io/GameMarket_Lazarenko/n/market.html?authuser=0"
+    },
+    {
+        title: "Пригоди Свинки Шмеппи 3",
+        author: "Кайданюк Ілля",
+        author_photo: "Кайданюк.jpg",
+        email: "23a_kiv@liceum.ztu.edu.ua",
+        group: "10-А-1",
+        image: "Кайданюк_гра.png",
+        game_github: "https://github.com/Nefenix/Phaser2ndGame",
+        game_pages: "https://nefenix.github.io/Phaser2ndGame/",
+        docs: "https://drive.google.com/drive/folders/1xlqWdx6uMj_5XWDlfl4EYZtAD6A8LGMq?usp=drive_link",
+        forms: "https://forms.gle/e4camX4k2MZvK9qf7",
+        gamemarket_github: "https://github.com/Nefenix/GameMarket_Kaidaniuk",
+        gamemarket_pages: "https://nefenix.github.io/GameMarket_Kaidaniuk/",
+        market_pages: "https://nefenix.github.io/GameMarket_Kaidaniuk/market.html"
+    },
+    {
+        title: "Win to Survive",
+        author: "Марценюк Вадим",
+        author_photo: "Марценюк.jpg",
+        email: "23a_mvp@liceum.ztu.edu.ua",
+        group: "10-А-1",
+        image: "Марценюк_гра.png",
+        game_github: "https://github.com/JokerBrando/Phaser1stGame",
+        game_pages: "https://jokerbrando.github.io/Phaser1stGame/",
+        docs: "https://docs.google.com/document/d/1IsOcb3t2cARhpDgYS-dLoLIk3PGzBkk5SObT-1cRxzc/edit?usp=classroom_web&authuser=0",
+        forms: "https://forms.gle/s9RA2QcpfpFsY3Mt9?authuser=0",
+        gamemarket_github: "https://github.com/JokerBrando/GameMarket_Martseniuk_Vadym",
+        gamemarket_pages: "https://jokerbrando.github.io/GameMarket_Martseniuk_Vadym/",
+        market_pages: "https://jokerbrando.github.io/GameMarket_Martseniuk_Vadym/market.html"
+    },
+]
+//id = "items"
+let itemsDiv = document.getElementById('items');
+if(itemsDiv){
+    ItemsArray
+    .sort((a,b)=>a.author.localeCompare(b.author))
+    .forEach((item, index)=>{
+        itemsDiv.innerHTML += 
+        `<div class="item">
+                <!--Name-->
+                <div class="item-title">${item.title}</div>
+                <div>${item.author}</div>
+                <!--Info-->
+                <div>${item.group}</div>
+                <div><a href="${item.email}" target="_blank">${item.email}</a></div>
+                <!--Photo-->
+                <div class="photos">
+                <img src="img_st/${item.author_photo}" class="item-photo">
+                <img src="img_st/${item.image}" class="item-image">
+                </div>
+                <!--Game-->
+                <div class="links">
+                <div><a href="${item.game_github}" target="_blank">GitHub гри</a></div>
+                <div><a href=${item.game_pages} target="_blank">Гра</a></div>
+                <div><a href="${item.docs}" target="_blank">Документація</a></div>
+                <div><a href=">${item.forms}" target="_blank">Опитування</a></div>
+                <div><a href="${item.gamemarket_github}" target="_blank">GitHub маркету</a></div>
+                <div><a href="${item.gamemarket_pages}" target="_blank">Маркет ігор початковий</a></div>
+                <div><a href="${item.market_pagesb}" target="_blank">Маркет продуктів</a></div>
+                </div>
+        </div>`
+    })
+   
+    //<p><a href="mailto:${item.email}?subject=Питання&body=Доброго дня" target="_blank" class="price">Написати</p>
+    /*console.log('itemsDiv')
+    console.log('ClassList:', itemsDiv.classList);
+    console.log('Id:', itemsDiv.id);
+    console.log('ClientWidth:', itemsDiv.clientWidth);
+    console.log('InnerHTML:', itemsDiv.innerHTML);*/
+    //Add our products
+    /*for( let i=0; i<=101; i++) {
+        itemsDiv.innerHTML += '<div class="item"></div>';
+    }*/
+    
+} else{
+    console.log('Not Found');
+}
+
+// /*//Massive Start
+// let ItemsArray = [
+//     'Газонокосарка 43',
+//     'Електричний тример 110',
+//     'Електрична газонокосарка 32',
+//     'Акумуляторний оприскувач 12 N',
+//     'Газонокосарка 430',
+//     'Електричний тример 130',
+//     'Електрична газонокосарка 320',
+//     'Акумуляторний оприскувач 12 E',
+// ]
+// ItemsArray.sort().forEach((item, index)=>{
+//     console.log(index + '-й елемент:', item)
+// })*/
+// /*ItemsArray=ItemsArray.sort();
+
+// console.log(ItemsArray)
+// for(let i=0; i<ItemsArray.length;i++){
+//     console.log(i + '-й елемент:', ItemsArray[i])
+// }*/
+
+
+// /*{
+//     firstName:'Іван',
+//     lastName:'Севрук',
+//     age:60,
+//     subject:'Захист України',
+//     photo:'https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/sevruk-ivan-hryhorovych-683x1024.jpg',
+//     url:'https://lyceum.ztu.edu.ua/team/sevruk-i-h/',
+//     email:"23@gmail.com",
+
+// // },
+// // {
+// //     firstName:'Наталія',
+// //     lastName:'Венцель',
+// //     age:20,
+// //     subject:'Директор',
+// //     photo:'https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/ventsel-nataliya-vasylivna-683x1024.jpg',
+// //     url:'https://lyceum.ztu.edu.ua/team/ventsel-n-v/',
+// // },
